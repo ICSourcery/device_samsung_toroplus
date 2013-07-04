7048b06f59614a12b9d89dbd0a28f4fc6ce91566
jake <jboyer0000@gmail.com>
2013-07-03 22:02:09 -0400

    use O3 and GCC 4.7 by default

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,3 +34,6 @@ BOARD_INVENSENSE_APPLY_COMPASS_NOISE_FILTER := true
 
 # libsec-ril_lte.so is broke due to a missing symbol in surfacecomposerclient
 TARGET_TOROPLUS_RADIO_FIX := true
+
+TARGET_GCC_VERSION := 4.7
+TARGET_USE_O3 := true
